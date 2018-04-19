d9128cb49f0dbf7e12809d677a674fb293add496
muhammadreza1044 <38530689+muhammadreza1044@users.noreply.github.com>
2018-04-19 20:29:03 +0700

    Update and rename README.md to projek.cpp

--- projek.cpp
+++ projek.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string.h>
+
   using namespace std;
-  
+
+  void array(){
+  for (i=0;i<10;i++){
+  for (j=0;j<10:j++){
+  string
+
   int main(){
   cout<<"Hello world"<<endl;
   
